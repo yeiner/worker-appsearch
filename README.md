e727b93696e8518deaf70b5122c949bfc934e947
Yeiner Morales <yeiner.morales@gmail.com>
2019-12-30 17:07:55 -0500

    Avance Readme, modifica tests config file :snake:

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # worker-appsearch
+
+## Requerimientos
+
+- **Linux**
+- **Python 3.5** (Lenguaje de programación)
+- **PIP3** (Python Package Index - Sistema de gestión de paquetes)
+- **Pipenv** (Flujo de trabajo en python)
+
+## Getting Started 
+
+Instalación dependencias python.
+
+```
+sudo pip3 install pipenv
+pipenv install
+```
+
+##### Configuración local.
+Archivo de configuración app-search **config/config.ini
+
+```
+; config.ini
+[DEFAULT]
+BEARER = bearer-app-search
+HOST_APPSEARCH = host-app-search
+HOST_IDENTIFIER =  host_identifier
+ENGINE_NAME = engine_name
+```
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,14 +1,24 @@
 import pytest
-import requests 
+import requests
+import configparser
+from pathlib import Path
 
-url = 'https://host-2je4fy.api.swiftype.com'
-header = {'Authorization': 'Bearer private-xnv4gm2rwyhxhbzzba8y827o'}
-engine = 'loe-promotions'
+path_config_file = Path.cwd() / 'config' / 'config.ini'
+config = configparser.ConfigParser()
+config.read(str(path_config_file))
+host_identifier = config['TESTING']['HOST_IDENTIFIER']
+host_appsearch = config['TESTING']['HOST_APPSEARCH']
+api_key = config['TESTING']['BEARER']
+engine_name = config['TESTING']['ENGINE_NAME']
+
+url = 'https://' + host_identifier + '.' +  host_appsearch
+header = {'Authorization': 'Bearer ' + api_key}
+
 
 def test_conection_list_documents():
-    r = requests.get(url + '/api/as/v1/engines/' + engine + '/documents/list', headers=header)
+    r = requests.get(url + '/api/as/v1/engines/' + engine_name + '/documents/list', headers=header)
     assert r.status_code == 200
 
 def test_conection_engine():
-    r = requests.get(url + '/api/as/v1/engines/' + engine , headers=header)
+    r = requests.get(url + '/api/as/v1/engines/' + engine_name , headers=header)
     assert r.status_code ==  200
